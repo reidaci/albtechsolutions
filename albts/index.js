757518e7aa69ea3de5e637b3b5096cc36d7622b1
Rei Daci <reiz.daci@outlook.com>
2023-12-01 16:23:28 +0100

    deploy to firebase

---
+++ albts/index.js
@@ -0,0 +1,31 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+admin.initializeApp();
+require("dotenv").config();
+
+const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
+
+
+exports.sendEmailNotification = functions.firestore.document("EMAILS/{docId}")
+    .onCreate((snap, _ctx) => {
+      const data=snap.data();
+
+      const authData = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: SENDER_EMAIL,
+          pass: SENDER_PASSWORD,
+        },
+      });
+      authData.sendMail({
+        from: "reiz.albania@gmail.com",
+        to: `{data.email}`,
+        subject: "Hello rei",
+        text: `${data.email}`,
+        html: `${data.email}`,
+      // eslint-disable-next-line max-len
+      }).then((res) => console.log("Successfully sent that email")).catch((err) => console.log(err));
+    });
